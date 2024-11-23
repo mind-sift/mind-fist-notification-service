97f4891a5cdf7320ef9a683358daabea6294dbba
matias-ovalle-toteat <matias@toteat.com>
2024-11-23 16:18:38 -0300

    feat(notification_stream): add Push model and dismiss_push function for handling notifications

--- src/notification_stream.py
+++ src/notification_stream.py
@@ -1,10 +1,25 @@
-import asyncio
 import json
 import logging
 import os
 
 import requests
 import websockets
+from pydantic import BaseModel
+
+
+class Push(BaseModel):
+    type: str
+    source_user_iden: str
+    source_device_iden: str
+    client_version: int
+    icon: str
+    title: str
+    body: str
+    application_name: str
+    package_name: str
+    notification_id: str
+    notification_tag: str
+
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
@@ -35,13 +50,47 @@ def list_pushes():
     print("response json:", response.json())
 
 
+def dismiss_push(push: Push):
+    KEY = os.getenv("PUSHBULLET_API_KEY")
+
+    headers = {
+        'Access-Token': KEY,
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'push': {
+            'notification_id': push.notification_id,
+            "notification_tag": push.notification_tag,
+            'package_name': push.package_name,
+            'source_user_iden': push.source_user_iden,
+            'type': 'dismissal',
+        },
+        'type': 'push',
+    }
+
+    response = requests.post(
+        'https://api.pushbullet.com/v2/ephemerals', headers=headers, json=json_data
+    )
+
+    print("notification dismiss response status code", response.status_code)
+
+
 async def process(msg):
     event = json.loads(msg)
     if event.get("type") == "push":
-        p = event.get("push", {})
+        p_json_data = event.get("push", {})
+        try:
+            push = Push(**p_json_data)
+        except ValueError as e:
+            print(e.errors())
+            print()
+            print(p_json_data)
+            return
         print()
-        print(p)
+        print(push.model_dump())
         print()
+        dismiss_push(push)
 
 
 async def stream():
